4f2be6cfa50c05c38d1d46649ac2e9145f3fb576
amirkle27 <amirkle@gmail.com>
2024-08-24 13:25:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ def play_game ():
         if ans in ["yes","y"]:
             print("Lets go then!")
             print("Player no. 1,")
-            num1 = game.check_validity()
+            choice1: str = input("Please choose: Rock, Paper, Scissors")
+            num1 = game.check_validity(choice1)
             print("Player no. 2,")
-            num2 = game.check_validity()
+            choice2: str = input("Please choose: Rock, Paper, Scissors")
+            num2 = game.check_validity(choice2)
             (game.check_winner(num1,num2))
             if game.check_winner(num1,num2) == 0:
                 print("OH! It's a Draw!")
@@ -17,14 +19,11 @@ def play_game ():
                 print("Player no. 1 won! Congratulations!")
             else:
                 print("Player no. 2 won! Congratulations!")
-            ans = input("Play Again?")
+            ans = input("Play Again?").lower().strip()
         elif ans in ["no", "n"]:
             print("Never mind. Some other time perhaps...")
             break
         else:
             ans = input("I didn't quite get your answer. Could you repeat that?").lower().strip()
-
-
-
 
 play_game()
