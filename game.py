bf4e4bf7cf715fac8cc7442d012a64d788fab55a
amirkle27 <amirkle@gmail.com>
2024-08-24 13:26:31 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,13 @@
 def check_validity (choice: str = "") -> int:
-    choice: str = input("Please choose: Rock, Paper, Scissors").lower()
-    match choice:
+    match choice.lower().strip():
         case "rock":
-            num:int = 2
+            return  2
         case "scissors":
-            num:int = 1
+            return  1
         case "paper":
-            num:int = 0
+            return  0
         case _:
             raise ValueError ("Illegal Game Option")
-    return num
-
 
 def check_winner (player1_choice: int, player2_choice: int) -> int:
     verdict: int =  1 if player1_choice-1 == player2_choice or player1_choice+2 == player2_choice else 2 if player2_choice-1 == player1_choice or player2_choice+2 == player1_choice else 0 if player1_choice==player2_choice else None
